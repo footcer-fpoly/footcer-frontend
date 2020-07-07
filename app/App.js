d54ca8a89eb8d8af7982b6107b82d16ff62d65b7
= <=>
2020-07-07 15:02:40 +0700

    add file note in folder

--- app/App.js
+++ app/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-
 export default class App extends Component {
-  render() {
-    return (
-      <View>
-          <Text>Haha</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View>
+                <Text>Haha</Text>
+            </View>
+        );
+    }
 }
