e45ab517e65134cdf2f3668b242a0ea6f2f60949
duonghaidang <dangapagdang@gmail.com>
2020-07-17 15:19:16 +0700

    demo git

--- index.js
+++ index.js
@@ -5,5 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './app/screens/ProfileDetailScreen';
 import {name as appName} from './app.json';
+//hahhâh
 
 AppRegistry.registerComponent(appName, () => App);
